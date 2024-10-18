a3a47b47054f7d3323b48c75d0a98fe07ebc2900
MarioLeon1 <80595580+MarioLeon1@users.noreply.github.com>
2024-10-18 12:41:50 +0200

    segundo commit

--- Makefile
+++ Makefile
@@ -3,3 +3,13 @@ run:
 
 test:
 	python -m unittest discover -s tests
+
+lint:
+	flake8 .
+
+coverage:
+	coverage run -m unittest discover -s tests
+	coverage report -m
+
+trivy:
+	trivy fs .
--- tests/test_functional.py
+++ tests/test_functional.py
@@ -7,10 +7,14 @@ class TestAnimalAPIFunctional(unittest.TestCase):
         self.app = app.test_client()
 
     def test_animal_rugir(self):
+        # Crear un animal en la base de datos de pruebas
         self.app.post('/animals', json={'name': 'Tigre', 'action': 'rugir'})
+        
+        # Realizar la solicitud POST al endpoint de rugir
         response = self.app.post('/animals/1/rugir')
-        self.assertEqual(response.status_code, 200)
-        self.assertIn('está rugiendo', str(response.data))
+        
+        # Decodificar la respuesta de bytes a una cadena usando UTF-8
+        self.assertIn('está rugiendo', response.data.decode('utf-8'))
 
 if __name__ == '__main__':
     unittest.main()
